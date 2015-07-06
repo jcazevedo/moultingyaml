23e905931112c88a6cbf58397ca3d5d3821eae20
Rui Gonçalves <ruippeixotog@gmail.com>
2015-07-06 18:37:12 +0100

    Rename type parameters in YamlFormat classes

--- src/main/scala/net/jcazevedo/moultingyaml/YamlFormat.scala
+++ src/main/scala/net/jcazevedo/moultingyaml/YamlFormat.scala
@@ -3,29 +3,29 @@ package net.jcazevedo.moultingyaml
 import scala.annotation.implicitNotFound
 
 @implicitNotFound(msg =
-  "Cannot find YamlReader or YamlFormat type class for ${T}")
-trait YamlReader[T] {
-  def read(json: YamlValue): T
+  "Cannot find YamlReader or YamlFormat type class for ${A}")
+trait YamlReader[A] {
+  def read(json: YamlValue): A
 }
 
 object YamlReader {
-  implicit def func2Reader[T](f: YamlValue => T): YamlReader[T] =
-    new YamlReader[T] {
+  implicit def func2Reader[A](f: YamlValue => A): YamlReader[A] =
+    new YamlReader[A] {
       def read(json: YamlValue) = f(json)
     }
 }
 
 @implicitNotFound(msg =
-  "Cannot find YamlWriter or YamlFormat type class for ${T}")
-trait YamlWriter[T] {
-  def write(obj: T): YamlValue
+  "Cannot find YamlWriter or YamlFormat type class for ${A}")
+trait YamlWriter[A] {
+  def write(obj: A): YamlValue
 }
 
 object YamlWriter {
-  implicit def func2Writer[T](f: T => YamlValue): YamlWriter[T] =
-    new YamlWriter[T] {
-      def write(obj: T) = f(obj)
+  implicit def func2Writer[A](f: A => YamlValue): YamlWriter[A] =
+    new YamlWriter[A] {
+      def write(obj: A) = f(obj)
     }
 }
 
-trait YamlFormat[T] extends YamlReader[T] with YamlWriter[T]
+trait YamlFormat[A] extends YamlReader[A] with YamlWriter[A]
