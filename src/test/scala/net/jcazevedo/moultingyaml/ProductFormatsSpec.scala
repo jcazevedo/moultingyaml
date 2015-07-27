0fd2b192cc827bbbd838539e91a08af9a846930e
Joao Azevedo <joao.c.azevedo@gmail.com>
2015-07-27 17:44:58 +0100

    Minor style fixes

--- src/test/scala/net/jcazevedo/moultingyaml/ProductFormatsSpec.scala
+++ src/test/scala/net/jcazevedo/moultingyaml/ProductFormatsSpec.scala
@@ -8,9 +8,10 @@ class ProductFormatsSpec extends Specification {
   case class Test2(a: Int, b: Option[Double])
   case class Test3[A, B](as: List[A], bs: List[B])
   case class Test4(t2: Test2)
-  case class Test5(a1: Int, a2: Int, a3: Int, a4: Int, a5: Int, a6: Int, a7: Int, a8: Int, a9: Int,
-                   a10: Int, a11: Int, a12: Int, a13: Int, a14: Int, a15: Int, a16: Int, a17: Int,
-                   a18: Int, a19: Int, a20: Int, a21: Int, a22: Int)
+  case class Test5(a1: Int, a2: Int, a3: Int, a4: Int, a5: Int, a6: Int,
+                   a7: Int, a8: Int, a9: Int, a10: Int, a11: Int, a12: Int,
+                   a13: Int, a14: Int, a15: Int, a16: Int, a17: Int, a18: Int,
+                   a19: Int, a20: Int, a21: Int, a22: Int)
   case class TestTransient(a: Int, b: Option[Double]) {
     @transient var c = false
   }
