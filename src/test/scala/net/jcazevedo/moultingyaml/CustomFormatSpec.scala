bdf0622ba826b0b6fbecec7c24b78d8b3258ac27
Joao Azevedo <joao.c.azevedo@gmail.com>
2015-07-29 22:07:26 +0100

    Add spec for testing `asYamlObject`

---
+++ src/test/scala/net/jcazevedo/moultingyaml/CustomFormatSpec.scala
@@ -0,0 +1,35 @@
+package net.jcazevedo.moultingyaml
+
+import org.specs2.mutable.Specification
+
+class CustomFormatSpec extends Specification with DefaultYamlProtocol {
+  case class MyType(name: String, value: Int)
+
+  implicit val MyTypeProtocol = new YamlFormat[MyType] {
+    def read(yaml: YamlValue) =
+      yaml.asYamlObject.getFields(
+        YamlString("name"), YamlString("value")) match {
+          case Seq(YamlString(name), YamlNumber(value: Int)) =>
+            MyType(name, value)
+          case _ => deserializationError("Expected fields: 'name' (YAML string) and 'value' (YAML number)")
+        }
+
+    def write(obj: MyType) =
+      YamlObject(
+        YamlString("name") -> YamlString(obj.name),
+        YamlString("value") -> YamlNumber(obj.value))
+  }
+
+  "A custom YamlFormat built with 'asYamlObject'" should {
+    val value = MyType("bob", 42)
+
+    "correctly deserialize valid YAML content" in {
+      """name: bob
+        |value: 42""".stripMargin.parseYaml.convertTo[MyType] mustEqual value
+    }
+
+    "support full round-trip (de)serialization" in {
+      value.toYaml.convertTo[MyType] mustEqual value
+    }
+  }
+}
